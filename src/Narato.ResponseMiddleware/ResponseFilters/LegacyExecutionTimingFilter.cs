89472b9d7300df14348afafd9f9751aeab13f081
Nicolaas Van Bladel <nicolaas.vanbladel@narato.be>
2017-05-24 11:03:52 +0200

    added LegacyExecutionTimingFilter

---
+++ src/Narato.ResponseMiddleware/ResponseFilters/LegacyExecutionTimingFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Narato.ResponseMiddleware.Models.Legacy.Models;
+using System;
+using System.Diagnostics;
+
+namespace Narato.ResponseMiddleware.ResponseFilters
+{
+    public class LegacyExecutionTimingFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            context.HttpContext.Items.Add("api_timing", sw);
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+
+            var sw = context.HttpContext.Items["api_timing"] as Stopwatch;
+
+            if (sw != null)
+            {
+                sw.Stop();
+                var objectResult = context.Result as ObjectResult;
+
+                if (objectResult != null && objectResult.Value is Response)
+                {
+                    (objectResult.Value as Response).Generation.Duration = sw.ElapsedMilliseconds;
+                    (objectResult.Value as Response).Generation.TimeStamp = DateTime.Now;
+                }
+            }
+        }
+    }
+}
